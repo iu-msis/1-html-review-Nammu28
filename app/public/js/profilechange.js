c716e8f26aa297c8bf72ad3f58672aef5b73e2f8
Nammu28 <namratamahesh28@gmail.com>
2021-09-16 12:19:22 -0400

    Cleaned up the date!
    Cleaned up the date and removed the para

--- app/public/js/profilechange.js
+++ app/public/js/profilechange.js
@@ -3,7 +3,7 @@ const Profile = {
         return{
                 "result":{},
 
-
+                "date":{},
 
             
         }
@@ -15,11 +15,12 @@ const Profile = {
 
 
 
-
+    
 
     },
-    //str = result.dob.date,
-    //str.trimRight(),
+
+    
+    
     created() { // after vue instance created, the function is called 
         fetch('https://randomuser.me/api') // Getting the data from the site, opens webpage in background - returns a promise - either do then or catch
         .then( response => response.json())   //return response in json format, creates a method - it returns a promise object
@@ -28,7 +29,24 @@ const Profile = {
                 console.log(json); // put it in console log
                 this.result = json.results[0]; // updaing the data value - vue recognizes it changed and updates it
                 console.log(this.result);
-                
+
+                //Convert Date to look pretty
+                let str = this.result.dob.date;
+                //Split the string '-'
+                let str1= str.split("-");
+                console.log(str1);
+                //Grab the month
+                let month = str1[1]
+                //grab the year
+                let year = str1[0]
+                // //Trim down day/time to just day
+                let day = str1[2].substring(0,2)
+                // str1.substring(0,11)
+
+                // //combine
+                 str2= year + "-"+ month + "-" +  day
+                this.date = str2
+
             }
         );
         // .catch( (error) => console.error(error)/*;}*/ ) /*;*/ // Any error is caught
