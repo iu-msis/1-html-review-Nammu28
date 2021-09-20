f1994951aac1d94708d8dca4338214052584ad8e
Nammu28 <namratamahesh28@gmail.com>
2021-09-20 12:28:44 -0400

    Added refresh button
    Made sure the refresh button only changes the results

--- app/public/js/profilechange.js
+++ app/public/js/profilechange.js
@@ -18,10 +18,7 @@ const Profile = {
     
 
     },
-
-    
-    
-    created() { // after vue instance created, the function is called 
+    methods:{ fetchUserData()  {
         fetch('https://randomuser.me/api') // Getting the data from the site, opens webpage in background - returns a promise - either do then or catch
         .then( response => response.json())   //return response in json format, creates a method - it returns a promise object
         .then(
@@ -29,7 +26,6 @@ const Profile = {
                 console.log(json); // put it in console log
                 this.result = json.results[0]; // updaing the data value - vue recognizes it changed and updates it
                 console.log(this.result);
-
                 //Convert Date to look pretty
                 let str = this.result.dob.date;
                 //Split the string '-'
@@ -47,6 +43,27 @@ const Profile = {
                  str2= year + "-"+ month + "-" +  day
                 this.date = str2
 
+                
+            }
+        );
+
+    }
+
+    },
+
+
+    
+    
+    created() { // after vue instance created, the function is called 
+        fetch('https://randomuser.me/api') // Getting the data from the site, opens webpage in background - returns a promise - either do then or catch
+        .then( response => response.json())   //return response in json format, creates a method - it returns a promise object
+        .then(
+            (json) => {
+                console.log(json); // put it in console log
+                this.result = json.results[0]; // updaing the data value - vue recognizes it changed and updates it
+                console.log(this.result);
+
+                
             }
         );
         // .catch( (error) => console.error(error)/*;}*/ ) /*;*/ // Any error is caught
