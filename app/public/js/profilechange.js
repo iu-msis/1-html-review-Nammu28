d8ca0b1ad7a53702b32a2218729b4f9facb135d2
Nammu28 <namratamahesh28@gmail.com>
2021-09-21 11:06:34 -0400

    Comments
    Commented out code for assignment due on Friday to avoid confusion

--- app/public/js/profilechange.js
+++ app/public/js/profilechange.js
@@ -18,7 +18,29 @@ const Profile = {
     
 
     },
-    methods:{ fetchUserData()  {
+    // methods:{ fetchUserData()  {
+    //     fetch('https://randomuser.me/api') // Getting the data from the site, opens webpage in background - returns a promise - either do then or catch
+    //     .then( response => response.json())   //return response in json format, creates a method - it returns a promise object
+    //     .then(
+    //         (json) => {
+    //             console.log(json); // put it in console log
+    //             this.result = json.results[0]; // updaing the data value - vue recognizes it changed and updates it
+    //             console.log(this.result);
+    //             //Convert Date to look pretty
+               
+
+                
+    //         }
+    //     );
+
+    // }
+
+    // },
+
+
+    
+    
+    created() { // after vue instance created, the function is called 
         fetch('https://randomuser.me/api') // Getting the data from the site, opens webpage in background - returns a promise - either do then or catch
         .then( response => response.json())   //return response in json format, creates a method - it returns a promise object
         .then(
@@ -26,7 +48,6 @@ const Profile = {
                 console.log(json); // put it in console log
                 this.result = json.results[0]; // updaing the data value - vue recognizes it changed and updates it
                 console.log(this.result);
-                //Convert Date to look pretty
                 let str = this.result.dob.date;
                 //Split the string '-'
                 let str1= str.split("-");
@@ -42,27 +63,6 @@ const Profile = {
                 // //combine
                  str2= year + "-"+ month + "-" +  day
                 this.date = str2
-
-                
-            }
-        );
-
-    }
-
-    },
-
-
-    
-    
-    created() { // after vue instance created, the function is called 
-        fetch('https://randomuser.me/api') // Getting the data from the site, opens webpage in background - returns a promise - either do then or catch
-        .then( response => response.json())   //return response in json format, creates a method - it returns a promise object
-        .then(
-            (json) => {
-                console.log(json); // put it in console log
-                this.result = json.results[0]; // updaing the data value - vue recognizes it changed and updates it
-                console.log(this.result);
-
                 
             }
         );
